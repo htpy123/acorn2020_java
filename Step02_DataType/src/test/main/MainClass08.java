b54bd8a3c2f835d958cd8d07e9efaf9ec55f51bb
jeageun <htpy123@naver.com>
2020-05-28 16:26:10 +0900

    MainClass08.java 추가

---
+++ Step02_DataType/src/test/main/MainClass08.java
@@ -0,0 +1,16 @@
+package test.main;
+
+public class MainClass08 {
+	public static void main(String[] args) {
+		//String type
+		String str="abcde12345";
+		//문자열의 길이
+		int size = str.length();
+		//5번째 인덱스의 문자 1개 (char)
+		char ch = str.charAt(5);
+		//소문자를 모두 대문자로 변환한 문자열 얻어내기
+		String result = str.toUpperCase();
+		
+		System.out.println(size+"+"+ch+"+"+result);
+	}
+}
