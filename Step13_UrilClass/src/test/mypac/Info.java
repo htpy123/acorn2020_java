e06d47e87ac7ef08079b79f05ee58a3c7f80084b
jeageun <htpy123@naver.com>
2020-06-10 16:04:57 +0900

    Step13_UrilClass 수정

---
+++ Step13_UrilClass/src/test/mypac/Info.java
@@ -0,0 +1,19 @@
+package test.mypac;
+
+public class Info {
+	public int num;
+	public String name;
+	public String addr;
+	
+	
+	
+	
+	
+	
+//	public Info(int num, String name, String addr) {
+//		this.num=num;
+//		this.name=name;
+//		this.addr=addr;
+//	}
+
+}
